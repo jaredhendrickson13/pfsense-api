e35be4724524de9ef8f6dd5f332be6837e24b52d
Jared Hendrickson <jhendrickson@securitymetrics.com>
2020-01-21 15:11:53 -0700

    Added service start, stop, and restart enpoints for DNS Resolver (unbound), updated pkg-plist and Makefile

---
+++ pfSense-pkg-API/files/usr/local/www/api/v1/services/unbound/stop/index.php
@@ -0,0 +1,42 @@
+<?php
+
+// IMPORTS--------------------------------------------------------------------------------------------------------------
+require_once("config.inc");
+require_once("auth.inc");
+require_once("functions.inc");
+require_once("api.inc");
+
+// HEADERS--------------------------------------------------------------------------------------------------------------
+api_runtime_allowed();    // Check that our configuration allows this API call to run first
+header('Content-Type: application/json');
+header("Referer: no-referrer");
+
+// VARIABLES------------------------------------------------------------------------------------------------------------
+global $config, $api_resp, $client_params;
+$read_only_action = true;    // Set whether this action requires read only access
+$req_privs = array("page-all", "page-services-dnsresolver");    // Array of privs allowed
+$http_method = $_SERVER['REQUEST_METHOD'];    // Save our HTTP method
+$unbound_array = array();    // Init our return array
+
+// RUN TIME-------------------------------------------------------------------------------------------------------------
+// Check that client is authenticated and authorized
+if (api_authorized($req_privs, $read_only_action)) {
+    // Check that our HTTP method is POST (UPDATE)
+    if ($http_method === 'POST') {
+        $stop_serv = service_control_stop("unbound", []);    // Stop our service
+        // Print our JSON response
+        $api_resp = array("status" => "ok", "code" => 200, "return" => 0, "message" => "service unbound has been stopped");
+        http_response_code(200);
+        echo json_encode($api_resp) . PHP_EOL;
+        die();
+    } else {
+        $api_resp = array("status" => "bad request", "code" => 400, "return" => 2, "message" => "invalid http method");
+        http_response_code(400);
+        echo json_encode($api_resp) . PHP_EOL;
+        die();
+    }
+} else {
+    http_response_code(401);
+    echo json_encode($api_resp) . PHP_EOL;
+    die();
+}
