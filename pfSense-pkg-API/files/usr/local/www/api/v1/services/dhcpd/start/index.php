53203f087775c53abd9f134d613dfb0509e2bc57
Jared Hendrickson <jhendrickson@securitymetrics.com>
2020-01-22 19:49:37 -0700

    Add start, stop, and restart endpoints for all services (/services/start, /services/stop, /services/restart), changed format of individual start, stop, restart service enpoints to self identify the service and action being requested. Updated pkg-plist and Makefile

--- pfSense-pkg-API/files/usr/local/www/api/v1/services/dhcpd/start/index.php
+++ pfSense-pkg-API/files/usr/local/www/api/v1/services/dhcpd/start/index.php
@@ -14,20 +14,45 @@ header("Referer: no-referrer");
 // VARIABLES------------------------------------------------------------------------------------------------------------
 global $config, $api_resp, $client_params;
 $read_only_action = true;    // Set whether this action requires read only access
-$req_privs = array("page-all", "page-services-dhcp");    // Array of privs allowed
+$req_privs = array("page-all", "page-status-services");    // Array of privs allowed
 $http_method = $_SERVER['REQUEST_METHOD'];    // Save our HTTP method
+$uri_parse = explode("/", $_SERVER["REQUEST_URI"]);    // Save our URI
+$service = $uri_parse[4];    // Save our service name
+$action = $uri_parse[5];    // Save our action (start, stop, restart)
 
 // RUN TIME-------------------------------------------------------------------------------------------------------------
 // Check that client is authenticated and authorized
 if (api_authorized($req_privs, $read_only_action)) {
     // Check that our HTTP method is POST (UPDATE)
     if ($http_method === 'POST') {
-        $stop_serv = service_control_start("dhcpd", []);    // Start our service
-        // Print our JSON response
-        $api_resp = array("status" => "ok", "code" => 200, "return" => 0, "message" => "service dhcpd has been started");
-        http_response_code(200);
-        echo json_encode($api_resp) . PHP_EOL;
-        die();
+        # Check our aciton
+        if ($action === "start") {
+            $set_service = service_control_start($service, []);    // Start our service
+            $act_str = "started";
+        } elseif ($action === "stop") {
+            $set_service = service_control_stop($service, []);    // Stop our service
+            $act_str = "stopped";
+        } elseif ($action === "restart") {
+            $set_service = service_control_restart($service, []);    // Restart our service
+            $act_str = "restarted";
+        } else {
+            $act_failed = true;
+        }
+        // Check if our action succeeded or failed
+        if (!$act_failed) {
+            // Print our success response
+            $api_resp = array("status" => "ok", "code" => 200, "return" => 0);
+            $api_resp["message"] = $service." has been ".$act_str;
+            http_response_code(200);
+            echo json_encode($api_resp) . PHP_EOL;
+            die();
+        } else {
+            // Print our fail response
+            $api_resp = array("status" => "server error", "code" => 500, "return" => 1, "message" => "process failed");
+            http_response_code(500);
+            echo json_encode($api_resp) . PHP_EOL;
+            die();
+        }
     } else {
         $api_resp = array("status" => "bad request", "code" => 400, "return" => 2, "message" => "invalid http method");
         http_response_code(400);
