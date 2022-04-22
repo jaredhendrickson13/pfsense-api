ec7bdbaeae4d34cfda1f00d419087cfcd14d2e7a
Jared Hendrickson <jaredhendrickson13@gmail.com>
2022-04-22 01:03:52 -0600

    Finished conversion from Postman/Docgen to OpenAPI/Swagger

---
+++ pfSense-pkg-API/files/usr/local/www/api/documentation/swagger-initializer.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "/api/documentation/openapi.yml",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+  });
+
+  //</editor-fold>
+};
