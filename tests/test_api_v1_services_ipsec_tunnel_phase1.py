a3b243a23c6494d90e9f47dadf516573237261b6
Jared Hendrickson <jaredhendrickson13@gmail.com>
2022-08-22 18:38:49 -0600

    chore: support vip interfaces
    adds the is_vip() method to check if a specific value
    is a virtual IPs unique ID and changes the
    get_pfsense_if_id() method to use is_vip() when
    the include_carp flag is enabled.

---
+++ tests/test_api_v1_services_ipsec_tunnel_phase1.py
@@ -0,0 +1,29 @@
+# Copyright 2022 Jared Hendrickson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Script used to test the /api/v1/services/start endpoint."""
+import e2e_test_framework
+
+
+class APIE2ETestServicesStart(e2e_test_framework.APIE2ETest):
+    """Class used to test the /api/v1/services/start endpoint."""
+    uri = "/api/v1/services/start"
+    post_tests = [
+        {
+            "name": "Start all services",
+            "resp_time": 10
+        }
+    ]
+
+
+APIE2ETestServicesStart()
