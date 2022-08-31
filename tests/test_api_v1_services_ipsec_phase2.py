91331be079aa6ccf7d6ec32edc4b1cb5606c1966
Jared Hendrickson <jaredhendrickson13@gmail.com>
2022-08-30 22:34:32 -0600

    fix: fixed p2 uniqid locator

--- tests/test_api_v1_services_ipsec_phase2.py
+++ tests/test_api_v1_services_ipsec_phase2.py
@@ -651,7 +651,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
     put_tests = [
         {
             "name": "Update valid IPsec phase 2",
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -680,28 +680,28 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check ikeid exists constraint",
             "status": 400,
             "return": 2208,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {"ikeid": "INVALID"}
         },
         {
             "name": "Check mode options constraint",
             "status": 400,
             "return": 2220,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {"ikeid": 1, "mode": "INVALID"}
         },
         {
             "name": "Check localid type options constraint",
             "status": 400,
             "return": 2225,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {"ikeid": 1, "mode": "tunnel", "localid": {"type": "INVALID"}}
         },
         {
             "name": "Check localid address required constraint",
             "status": 400,
             "return": 2226,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -712,7 +712,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check localid address IP constraint when 'mode' is 'vti'",
             "status": 400,
             "return": 2227,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "vti",
@@ -723,7 +723,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check localid address IPv4 constraint when 'mode' is 'tunnel'",
             "status": 400,
             "return": 2228,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -734,7 +734,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check localid address IPv6 constraint when 'mode' is 'tunnel6'",
             "status": 400,
             "return": 2229,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel6",
@@ -755,7 +755,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check localid IPv4 netbits minimum constraint when 'type' is 'network'",
             "status": 400,
             "return": 2231,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -766,7 +766,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check localid IPv4 netbits maxmimum constraint when 'type' is 'network'",
             "status": 400,
             "return": 2231,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -777,7 +777,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check localid IPv6 netbits minimum constraint when 'type' is 'network'",
             "status": 400,
             "return": 2232,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel6",
@@ -788,7 +788,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check localid IPv6 netbits maxmimum constraint when 'type' is 'network'",
             "status": 400,
             "return": 2232,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel6",
@@ -799,7 +799,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check natlocalid type required constraint",
             "status": 400,
             "return": 2233,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -811,7 +811,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check natlocalid type must match localid type constraint",
             "status": 400,
             "return": 2235,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -823,7 +823,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check natlocalid address required constraint",
             "status": 400,
             "return": 2236,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -835,7 +835,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check natlocalid address IPv4 constraint when 'mode' is set to 'tunnel'",
             "status": 400,
             "return": 2237,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -847,7 +847,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check natlocalid address IPv6 constraint when 'mode' is set to 'tunnel6'",
             "status": 400,
             "return": 2238,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel6",
@@ -859,7 +859,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check natlocalid netbits required constraint when 'type' is 'network'",
             "status": 400,
             "return": 2239,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -871,7 +871,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check natlocalid netbits matches localid netbits constraint",
             "status": 400,
             "return": 2240,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -883,7 +883,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check remoteid type required constraint",
             "status": 400,
             "return": 2241,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -895,7 +895,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check remoteid type options constraint",
             "status": 400,
             "return": 2242,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -907,7 +907,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check remoteid address required constraint",
             "status": 400,
             "return": 2243,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -919,7 +919,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check remoteid address IPv4 constraint when 'mode' is 'tunnel'",
             "status": 400,
             "return": 2244,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -931,7 +931,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check remoteid address IPv6 constraint when 'mode' is 'tunnel6'",
             "status": 400,
             "return": 2245,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel6",
@@ -943,7 +943,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check remoteid netbits required constraint when 'type' is 'network'",
             "status": 400,
             "return": 2246,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -955,7 +955,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check remoteid netbits minimum constraint when 'mode' is 'tunnel'",
             "status": 400,
             "return": 2247,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -967,7 +967,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check remoteid netbits maximum constraint when 'mode' is 'tunnel'",
             "status": 400,
             "return": 2247,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -979,7 +979,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check remoteid netbits minimum constraint when 'mode' is 'tunnel6'",
             "status": 400,
             "return": 2248,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel6",
@@ -991,7 +991,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check remoteid netbits maximum constraint when 'mode' is 'tunnel6'",
             "status": 400,
             "return": 2248,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel6",
@@ -1003,7 +1003,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check protocol options constraint",
             "status": 400,
             "return": 2222,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1016,7 +1016,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check encryption-algorithm-option type constraint",
             "status": 400,
             "return": 2250,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1030,7 +1030,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check encryption-algorithm-option minimum length constraint",
             "status": 400,
             "return": 2250,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1044,7 +1044,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check encryption-algorithm-option options constraint",
             "status": 400,
             "return": 2210,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1058,7 +1058,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check hash-algorithm-option type constraint",
             "status": 400,
             "return": 2252,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1073,7 +1073,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check hash-algorithm-option minimum length constraint",
             "status": 400,
             "return": 2252,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1088,7 +1088,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check hash-algorithm-option options constraint",
             "status": 400,
             "return": 2253,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1103,7 +1103,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check pfsgroup options constraint",
             "status": 400,
             "return": 2255,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1119,7 +1119,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check rekey_time numeric constraint",
             "status": 400,
             "return": 2193,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1136,7 +1136,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check rekey_time minimum constraint",
             "status": 400,
             "return": 2193,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1153,7 +1153,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check rand_time numeric constraint",
             "status": 400,
             "return": 2195,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1170,7 +1170,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check rand_time minimum constraint",
             "status": 400,
             "return": 2195,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1187,7 +1187,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check lifetime numeric constraint",
             "status": 400,
             "return": 2196,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1204,7 +1204,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check lifetime minimum constraint",
             "status": 400,
             "return": 2196,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1221,7 +1221,7 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
             "name": "Check lifetime greater than rekey_time constraint",
             "status": 400,
             "return": 2197,
-            "need_uniqid": True,
+            "needs_uniqid": True,
             "payload": {
                 "ikeid": 1,
                 "mode": "tunnel",
@@ -1264,10 +1264,11 @@ class APIE2ETestServicesIPsecPhase2(e2e_test_framework.APIE2ETest):
     def post_post(self):
         # Check if the second post test was done
         if len(self.post_responses) == 2:
-            for test in self.put_tests:
+            for i, test in enumerate(self.put_tests):
                 # Add the unique ID of the phase 2 entry if needed
                 if "needs_uniqid" in test and "payload" in test:
-                    test["payload"]["uniqid"] = self.post_responses[1]["data"]["uniqid"]
+                    self.put_tests[i]["payload"]["uniqid"] = self.post_responses[1]["data"]["uniqid"]
+            print(self.put_tests)
 
 
 APIE2ETestServicesIPsecPhase2()
