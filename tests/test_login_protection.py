d3b28da21e83af9089ff46e331af8aab46e4301b
Jared Hendrickson <jaredhendrickson13@gmail.com>
2022-10-07 08:25:25 -0600

    tests: just expect login protection timeout
    before the test_login_protection test expected requests to start failing at a
    specific point, but this does not work with all login protection configs and
    situations. it now just expects the requests to start to timeout after many
    failed auth attempts

--- tests/test_login_protection.py
+++ tests/test_login_protection.py
@@ -14,26 +14,22 @@ class APIE2ETestLoginProtection(e2e_test_framework.APIE2ETest):
         # Variables
         test_params = {"name": "Ensure login protection blocks many failed auth attempts"}
 
-        # Fail authentication 3 times to initiate the lockout
-        for _ in range(0, 3):
-            requests.get(
-                self.format_url("/api/v1/system/api"),
-                auth=("bad_username", "bad_password"),
-                verify=False
-            )
+        # Fail authentication many times to initiate the lockout
+        for _ in range(0, 5):
+            try:
+                requests.get(
+                    self.format_url("/api/v1/system/api"),
+                    auth=("bad_username", "bad_password"),
+                    verify=False,
+                    timeout=(5, 5)
+                )
+            except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectTimeout):
+                print(self.__format_msg__("GET", test_params, "Response is valid", mode="ok"))
+                sys.exit(0)
 
-        # Try another request and ensure it times out as expected
-        try:
-            requests.get(
-                self.format_url("/api/v1/system/api"),
-                auth=("bad_username", "bad_password"),
-                verify=False,
-                timeout=(5, 5)
-            )
-            print(self.__format_msg__("GET", test_params, "Expected lockout for too many failed requests"))
-            sys.exit(1)
-        except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectTimeout):
-            print(self.__format_msg__("GET", test_params, "Response is valid", mode="ok"))
+        # Test fails if we were not locked out
+        print(self.__format_msg__("GET", test_params, "Expected lockout for too many failed requests"))
+        sys.exit(1)
 
 
 APIE2ETestLoginProtection()
