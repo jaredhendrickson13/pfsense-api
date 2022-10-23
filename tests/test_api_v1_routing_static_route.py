d932b7733b3857a8d69caf3360078f03da185622
Jared Hendrickson <jaredhendrickson13@gmail.com>
2022-10-23 14:55:29 -0600

    tests: check that routes are actually in the table

--- tests/test_api_v1_routing_static_route.py
+++ tests/test_api_v1_routing_static_route.py
@@ -22,29 +22,159 @@ class APIE2ETestRoutingStaticRoute(e2e_test_framework.APIE2ETest):
     post_tests = [
         {
             "name": "Create static route",
-            "payload": {"network": "172.16.172.0/29", "gateway": "WAN_DHCP", "disabled": True, "descr": "E2E Test"}
-        }
+            "resp_time": 5,
+            "payload": {
+                "network": "1.2.3.4/32",
+                "gateway": "WAN_DHCP",
+                "disabled": False,
+                "descr": "E2E Test",
+                "apply": True
+            }
+        },
+        {
+            "name": "Check that static route is present on system",
+            "method": "POST",
+            "post_test_callable": "check_for_static_route_created",
+            "uri": "/api/v1/diagnostics/command_prompt",
+            "payload": {"shell_cmd": "netstat -rn"}
+        },
+        {
+            "name": "Check network required constraint",
+            "status": 400,
+            "return": 6000
+        },
+        {
+            "name": "Check network is CIDR or alias constraint",
+            "status": 400,
+            "return": 6001,
+            "payload": {"network": "INVALID"}
+        },
+        {
+            "name": "Check network minimum CIDR constraint",
+            "status": 400,
+            "return": 6002,
+            "payload": {"network": "1.2.3.4/0"}
+        },
+        {
+            "name": "Check network maximum IPv4 CIDR constraint",
+            "status": 400,
+            "return": 6002,
+            "payload": {"network": "1.2.3.4/33"}
+        },
+        {
+            "name": "Check network maximum IPv4 CIDR constraint",
+            "status": 400,
+            "return": 6002,
+            "payload": {"network": "0::/129"}
+        },
+        {
+            "name": "Check gateway required constraint",
+            "status": 400,
+            "return": 6003,
+            "payload": {"network": "1.2.3.4/32"}
+        },
+        {
+            "name": "Check gateway exists constraint",
+            "status": 400,
+            "return": 6004,
+            "payload": {"network": "1.2.3.4/32", "gateway": "INVALID"}
+        },
     ]
     put_tests = [
         {
             "name": "Update static route",
             "payload": {
                 "id": 0,
-                "network": "172.16.173.0/29",
+                "network": "4.3.2.1/32",
                 "gateway": "WAN_DHCP",
-                "disabled": True,
-                "descr": "Updated E2E Test"
+                "disabled": False,
+                "descr": "Updated E2E Test",
+                "apply": True
             },
             "resp_time": 5    # Allow a few seconds to reload the routing table
         },
+        {
+            "name": "Check that static route is updated on system",
+            "method": "POST",
+            "post_test_callable": "check_for_static_route_updated",
+            "uri": "/api/v1/diagnostics/command_prompt",
+            "payload": {"shell_cmd": "netstat -rn"}
+        },
+        {
+            "name": "Check ID required constraint",
+            "status": 400,
+            "return": 6005
+        },
+        {
+            "name": "Check ID exists constraint",
+            "status": 400,
+            "return": 6006,
+            "payload": {"id": "INVALID"}
+        },
+        {
+            "name": "Check network is CIDR or alias constraint",
+            "status": 400,
+            "return": 6001,
+            "payload": {"id": 0, "network": "INVALID"}
+        },
+        {
+            "name": "Check network minimum CIDR constraint",
+            "status": 400,
+            "return": 6002,
+            "payload": {"id": 0, "network": "1.2.3.4/0"}
+        },
+        {
+            "name": "Check network maximum IPv4 CIDR constraint",
+            "status": 400,
+            "return": 6002,
+            "payload": {"id": 0, "network": "1.2.3.4/33"}
+        },
+        {
+            "name": "Check network maximum IPv4 CIDR constraint",
+            "status": 400,
+            "return": 6002,
+            "payload": {"id": 0, "network": "0::/129"}
+        },
+        {
+            "name": "Check gateway exists constraint",
+            "status": 400,
+            "return": 6004,
+            "payload": {"id": 0, "network": "1.2.3.4/32", "gateway": "INVALID"}
+        }
     ]
     delete_tests = [
         {
             "name": "Delete static route",
             "payload": {"id": 0},
             "resp_time": 5    # Allow a few seconds to reload the routing table
+        },
+        {
+            "name": "Check ID required constraint",
+            "status": 400,
+            "return": 6005
+        },
+        {
+            "name": "Check ID exists constraint",
+            "status": 400,
+            "return": 6006,
+            "payload": {"id": "INVALID"}
         }
     ]
+
+    def check_for_static_route_created(self):
+        """Checks if the static route created by this test case is present on the remote system."""
+        # Check if the route we created for 1.2.3.4/32 is found in the routing table.
+        if "1.2.3.4" not in self.last_response.get("data", {}).get("cmd_output", ""):
+            raise AssertionError("1.2.3.4/32 was not found in the system table after creation via API")
+
+    def check_for_static_route_updated(self):
+        """Checks if the static route created by this test case is present on the remote system."""
+        # Ensure the route was correctly updated in the systems routing table.
+        if "4.3.2.1" not in self.last_response.get("data", {}).get("cmd_output", ""):
+            raise AssertionError("4.3.2.1/32 was not found in the system table after update via API")
+        # Ensure the old route is no longer present
+        if "1.2.3.4" in self.last_response.get("data", {}).get("cmd_output", ""):
+            raise AssertionError("old route to 1.2.3.4/32 was still present in the system table after update via API")
 
 
 APIE2ETestRoutingStaticRoute()
