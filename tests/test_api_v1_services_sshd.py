3d80339749586c9a07ba148ec24ab1604e441b42
Jared Hendrickson <jaredhendrickson13@gmail.com>
2022-10-31 23:45:39 -0600

    tests: improved sshd testing

--- tests/test_api_v1_services_sshd.py
+++ tests/test_api_v1_services_sshd.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Script used to test the /api/v1/services/sshd endpoint."""
+import socket
+import time
 import e2e_test_framework
 
 
@@ -21,16 +23,25 @@ class APIE2ETestServicesSSHd(e2e_test_framework.APIE2ETest):
     get_tests = [{"name": "Read SSHd configuration"}]
     put_tests = [
         {
-            "name": "Update and disable SSHd",
+            "name": "Start SSH on port 2222",
+            "post_test_callable": "check_ssh_reachable_on_2222",
             "req_data": {
-                "enable": False,
+                "enable": True,
                 "sshdkeyonly": "both",
                 "sshdagentforwarding": False,
                 "port": 2222
             }
         },
         {
-            "name": "Update and enabled SSHd",
+            "name": "Disable SSHd",
+            "post_test_callable": "check_ssh_reachable",
+            "req_data": {
+                "enable": False
+            }
+        },
+        {
+            "name": "Update and enable SSH on port 22",
+            "post_test_callable": "check_ssh_reachable_on_22",
             "req_data": {
                 "enable": True,
                 "sshdkeyonly": "disabled",
@@ -39,5 +50,41 @@ class APIE2ETestServicesSSHd(e2e_test_framework.APIE2ETest):
             }
         }
     ]
+
+    def check_ssh_reachable_on_22(self):
+        """Checks if SSH is reachable on port 22"""
+        # Allow a few seconds for SSH to start
+        time.sleep(3)
+
+        # Check if port 22 is not open like expected
+        if not self.is_port_open(22):
+            raise AssertionError("Expected SSH to be running on port 22")
+
+    def check_ssh_reachable_on_2222(self):
+        """Checks if SSH is reachable on port 2222"""
+        # Allow a few seconds for SSH to start
+        time.sleep(3)
+
+        # Check if port 2222 is not open like expected
+        if not self.is_port_open(2222):
+            raise AssertionError("Expected SSH to be running on port 2222")
+
+    def check_ssh_unreachable(self):
+        """Checks if SSH is unreachable"""
+        # Ensure neither of the previous ports we set are reachable.
+        if self.is_port_open(22) or self.is_port_open(2222):
+            raise AssertionError("Expected SSH to be unreachable because it is disabled.")
+
+    def is_port_open(self, port):
+        """Checks if a specified port is open on the target host."""
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        try:
+            sock.connect((self.args.host, int(port)))
+            sock.shutdown(2)
+            return True
+        except (TimeoutError, ConnectionError):
+            return False
+
 
 APIE2ETestServicesSSHd()
