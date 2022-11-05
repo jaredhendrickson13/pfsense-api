695f2b1e3e149409d858be872445b9d9e3ab5f41
Jared Hendrickson <jaredhendrickson13@gmail.com>
2022-11-04 21:22:33 -0600

    tests: check that hostname gets updated

--- tests/test_api_v1_system_hostname.py
+++ tests/test_api_v1_system_hostname.py
@@ -1,5 +1,11 @@
 """Script used to test the /api/v1/system/hostname endpoint."""
+import random
+
 import e2e_test_framework
+
+# Constants
+HOSTNAME_UPDATE = f"{random.randint(10, 99)}-pfsense-api-e2e"
+DOMAIN_UPDATE = "example.com"
 
 
 class APIE2ETestSystemHostname(e2e_test_framework.APIE2ETest):
@@ -9,7 +15,8 @@ class APIE2ETestSystemHostname(e2e_test_framework.APIE2ETest):
     put_tests = [
         {
             "name": "Update system hostname",
-            "req_data": {"hostname": "pfsense-api", "domain": "test.com"},
+            "post_test_callable": "is_hostname_updated",
+            "req_data": {"hostname": HOSTNAME_UPDATE, "domain": DOMAIN_UPDATE},
             "resp_time": 10    # Allow a few seconds for DNS services to reload
         },
         {
@@ -25,6 +32,15 @@ class APIE2ETestSystemHostname(e2e_test_framework.APIE2ETest):
             "req_data": {"hostname": "pfsense-api", "domain": "!@#$"}
         }
     ]
+
+    def is_hostname_updated(self):
+        """Checks if the system hostname was actually update using the 'hostname' shell command"""
+        # Local variables
+        hostname_out = self.pfsense_shell("hostname")
+
+        # Ensure the expected hostname is set
+        if hostname_out != f"{HOSTNAME_UPDATE}.{DOMAIN_UPDATE}":
+            raise AssertionError(f"Expected hostname to be set to {HOSTNAME_UPDATE}.{DOMAIN_UPDATE}")
 
 
 APIE2ETestSystemHostname()
