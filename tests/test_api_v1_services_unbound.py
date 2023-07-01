08ce4bfd2b226aa0a6a7c64927e2619338833b17
Jared Hendrickson <jaredhendrickson13@gmail.com>
2023-06-30 21:58:28 -0600

    feat: added PUT for /api/v1/services/unbound to update general options #313

--- tests/test_api_v1_services_unbound.py
+++ tests/test_api_v1_services_unbound.py
@@ -14,12 +14,35 @@
 """Script used to test the /api/v1/services/unbound endpoint."""
 import e2e_test_framework
 
+CUSTOM_OPTIONS_UPDATE = 'server:\nlocal-zone: "example.com" redirect\nlocal-data: "example.com 86400 IN A 10.1.2.3"'
 
 class APIE2ETestServicesUnbound(e2e_test_framework.APIE2ETest):
     """Class used to test the /api/v1/services/unbound endpoint."""
     uri = "/api/v1/services/unbound"
     get_privileges = ["page-all", "page-services-dnsresolver"]
     get_tests = [{"name": "Read the Unbound configuration"}]
+    put_tests = [
+        {
+            "name": "Check updating Unbound options",
+            "post_test_callable": "is_custom_options_updated",
+            "resp_time": 10,
+            "req_data": {
+                "enable": True,
+                "dnssec": True,
+                "custom_options": CUSTOM_OPTIONS_UPDATE,
+                "apply": True
+            }
+        },
+    ]
+
+    def is_custom_options_updated(self):
+        # Read the unbound.conf file
+        unbound_conf = self.pfsense_shell("cat /var/unbound/unbound.conf")
+
+        # Ensure custom options are found in the configuration
+        if CUSTOM_OPTIONS_UPDATE not in unbound_conf:
+            raise AssertionError("Expected custom_options to be present in /var/unbound/unbound.conf")
+
 
 
 APIE2ETestServicesUnbound()
