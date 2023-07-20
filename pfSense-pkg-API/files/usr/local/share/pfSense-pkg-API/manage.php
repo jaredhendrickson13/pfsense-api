5ab183f6efa2a51381f65ece4d6161ebbca32d47
Jared Hendrickson <jaredhendrickson13@gmail.com>
2023-07-20 17:10:20 -0600

    refactor: move build view function to APIView class

--- pfSense-pkg-API/files/usr/local/share/pfSense-pkg-API/manage.php
+++ pfSense-pkg-API/files/usr/local/share/pfSense-pkg-API/manage.php
@@ -51,28 +51,9 @@ function build_views() {
     foreach(glob("/etc/inc/api/views/*.inc") as $file) {
         # Import classes files and create object
         require_once($file);
-        $endpoint_class = str_replace(".inc", "", basename($file));
-        $endpoint_obj = new $endpoint_class();
-
-        # Specify the PHP code to write to the endpoints index.php file
-        $code = "<?php\nrequire_once('".$file."');\n(new ".$endpoint_class."())->listen();\n";
-
-        # Create directories and files corresponding with class
-        if (!is_null($endpoint_obj->url)) {
-            mkdir("/usr/local/www".$endpoint_obj->url, 0755, true);
-            file_put_contents(
-                "/usr/local/www".$endpoint_obj->url."/index.php",
-                $code
-            );
-        }
-
-        # Print success output if file now exists, otherwise output error and exit on non-zero code
-        if (!is_null($endpoint_obj->url) and is_file("/usr/local/www".$endpoint_obj->url."/index.php")) {
-            echo "Building ".$endpoint_class." view at URL \"".$endpoint_obj->url."\"... done.".PHP_EOL;
-        } else {
-            echo "Building ".$endpoint_class." view at URL \"".$endpoint_obj->url."\"... failed.".PHP_EOL;
-            exit(1);
-        }
+        $view_class = str_replace(".inc", "", basename($file));
+        $view_obj = new $view_class();
+        $view_obj->build_view();
     }
 }
 
